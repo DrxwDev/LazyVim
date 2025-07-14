4aa2a64681a016b6aca14594025ffef7f42aeec4
DrxwDev <drxwdev@outlook.es>
2025-07-14 10:34:27 -0400

    [Responsd with body]

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,10 +4,21 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
       "moyiz/blink-emoji.nvim",
+      -- "Exafunction/windsurf.vim", -- Codeium
     },
     version = "*", -- "1.*"
     lazy = true,
     opts = {
+      completion = {
+        menu = {
+          winblend = vim.o.pumblend,
+        },
+      },
+      signature = {
+        window = {
+          winblend = vim.o.pumblend,
+        },
+      },
 
       keymap = {
         preset = "enter",
@@ -16,18 +27,16 @@ return {
           ["<C-n>"] = "select_next_item",
           ["<C-Space>"] = "complete",
           ["<C-e>"] = "close",
-          ["<C-CR>"] = "accept",
+          ["<CR>"] = "accept",
         },
       },
 
       appearance = {
-        nerd_font_variant = "normal",
+        nerd_font_variant = "normal", -- "normal", "mono"
       },
 
-      completion = { documentation = { auto_show = true } },
-      signature = { enabled = true },
-
       sources = {
+        -- default = { "lsp", "path", "snippets", "buffer", "emoji", "sql", "codeium" },
         default = { "lsp", "path", "snippets", "buffer", "emoji" },
         providers = {
           emoji = {
@@ -36,10 +45,16 @@ return {
             score_offset = 15,
             opts = { insert = true },
             should_show_items = function()
-              return vim.tbl_contains({ "gitcommit" }, vim.o.filetype)
+              return vim.tbl_contains({ "gitcommit", "markdown" }, vim.o.filetype)
             end,
           },
-                  },
+          -- codeium = {
+          --   name = "codeium",
+          --   module = "blink.compat.source",
+          --   score_offset = 5, -- Adjust as needed
+          --   opts = {},
+          -- },
+        },
       },
       fuzzy = { implementation = "prefer_rust_with_warning" },
     },
