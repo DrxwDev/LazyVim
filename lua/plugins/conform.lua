fcb242eef25c60abf394e96affb2509c2f013b9d
DrxwDev <drxwdev@outlook.es>
2025-08-20 18:40:12 -0400

    Fixing the gitignore

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,22 +2,19 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      -- Web formats
       html = { "prettier" },
       javascript = { "prettier" },
       typescript = { "prettier" },
       javascriptreact = { "prettier" },
       typescriptreact = { "prettier" },
-      -- Optional: CSS/SCSS if needed
-      css = { "prettier" },
-      scss = { "prettier" },
+      css = { "prettier_css" },
+      scss = { "prettier_css" },
     },
     formatters = {
-      prettier = {
-        prepend_args = {
-          "--parser",
-          "typescript", -- For TS files
-        },
+      prettier = {}, -- TS/JS
+      prettier_css = {
+        command = "prettier",
+        args = { "--parser", "css" },
       },
     },
   },
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -120,16 +120,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "bash",
         "html",
         "javascript",
         "json",
         "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
         "query",
-        "regex",
         "tsx",
         "typescript",
         "vim",
